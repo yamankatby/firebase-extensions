9227c33e5ebdd2ff511ea2227f46299e87a4d07e
Yaman Katby <m.yaman.katby@gmail.com>
2022-11-22 11:28:46 +0300

    feat(generate-og-image): implement firestore collection

--- packages/generate-og-image/functions/src/config.ts
+++ packages/generate-og-image/functions/src/config.ts
@@ -1,15 +1,18 @@
 interface Config {
-  template: string;
+  templatesCollection?: string;
+  template?: string;
   width: number;
   height: number;
   imageFormat: "jpeg" | "png" | "webp";
 }
 
 const config: Config = {
-  template: process.env.TEMPLATE || "",
+  templatesCollection: process.env.TEMPLATES_COLLECTION,
+  template: process.env.TEMPLATE,
+  imageFormat: (process.env.IMAGE_FORMAT as Config["imageFormat"]) || "jpeg",
+
   width: Number(process.env.WIDTH) || 1200,
   height: Number(process.env.HEIGHT) || 630,
-  imageFormat: (process.env.IMAGE_FORMAT as Config["imageFormat"]) || "jpeg",
 };
 
 export default config;
--- packages/generate-og-image/functions/src/index.ts
+++ packages/generate-og-image/functions/src/index.ts
@@ -1,35 +1,83 @@
+import * as firebase from "firebase-admin";
 import * as functions from "firebase-functions";
-import handlebars from "handlebars";
+import handlebars, { TemplateDelegate } from "handlebars";
 import core from "puppeteer-core";
 import config from "./config";
 import getOptions from "./options";
+import { isValidHttpUrl } from "./utils";
 
-const compiledTemplate = handlebars.compile(config.template);
+firebase.initializeApp();
+
+const db = firebase.firestore();
+
+let compiledTemplate: TemplateDelegate | undefined;
+
+// If the template is a static HTML string, compile it once globally and reuse it
+if (config.template && !isValidHttpUrl(config.template)) {
+  compiledTemplate = handlebars.compile(config.template);
+}
 
 export const api = functions.handler.https.onRequest(async (req, res) => {
   const {
-    width = config.width,
-    height = config.height,
-    format = config.imageFormat,
-    ...rest
+    template: templateDocumentId = "default", // The document ID of the template to use, in case a Firestore collection is set
+    width = config.width, // The width of the image to generate
+    height = config.height, // The height of the image to generate
+    format = config.imageFormat, // The format of the image to generate
+    ...params
   } = req.query;
 
+  // If the template is a URL, refetch it on every request, so changes to the template are reflected immediately
+  if (config.template && isValidHttpUrl(config.template)) {
+    const template = await fetch(config.template).then((r) => r.text());
+    compiledTemplate = handlebars.compile(template);
+  }
+
+  // If the templates collection is set, fetch the template from Firestore
+  else if (config.templatesCollection) {
+    const templateDocument = await db
+      .collection(config.templatesCollection)
+      .doc(templateDocumentId as string)
+      .get();
+
+    const template = templateDocument.get("template") as string;
+
+    if (!template) {
+      res.status(404).send("Template not found");
+      return;
+    }
+
+    compiledTemplate = handlebars.compile(template);
+  }
+
+  // If no template is found, return an error
+  if (!compiledTemplate) {
+    res.status(404).send("Template not found");
+    return;
+  }
+
+  // Render the template
+  const html = compiledTemplate(params);
+
+  // Launch Puppeteer
   const options = await getOptions();
 
   const browser = await core.launch(options);
 
   const page = await browser.newPage();
 
+  // Set the viewport size
   await page.setViewport({ width: Number(width), height: Number(height) });
 
-  const content = compiledTemplate(rest);
+  // Set the content of the page
+  await page.setContent(html);
 
-  await page.setContent(content);
-
+  // Take a screenshot
   const buffer = await page.screenshot({ type: format as any });
 
+  // Close the browser
   await browser.close();
 
+  // Send the screenshot as a response
   res.set("Content-Type", `image/${format}`);
   res.send(buffer);
 });
