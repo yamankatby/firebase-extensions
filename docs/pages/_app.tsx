59e47e919e2fd6b30608e365e7e658272d476795
Yaman Katby <m.yaman.katby@gmail.com>
2022-11-24 19:04:30 +0300

    docs(*): setup next fonts

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,17 +1,32 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
 import DocsLayout from "../components/docs-layout";
 import "../styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const theme = extendTheme({
+  fonts: {
+    body: "var(--font-inter)",
+    heading: "var(--font-inter)",
+  },
+});
 
 export const githubUrl =
   "https://github.com/yamankatby/firestore-connect-document";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <DocsLayout>
-        <Component {...pageProps} />
-      </DocsLayout>
-    </ChakraProvider>
+    <main className={`${inter.variable} font-sans`}>
+      <ChakraProvider theme={theme}>
+        <DocsLayout>
+          <Component {...pageProps} />
+        </DocsLayout>
+      </ChakraProvider>
+    </main>
   );
 }
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
