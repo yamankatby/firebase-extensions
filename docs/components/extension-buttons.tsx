4bec752769728ac45bb6eccf343c33f6037e05ce
Yaman Katby <m.yaman.katby@gmail.com>
2022-11-24 16:45:15 +0300

    docs(*): setup chakra ui

---
+++ docs/components/extension-buttons.tsx
@@ -0,0 +1,39 @@
+import { Button, HStack, Tooltip } from "@chakra-ui/react";
+import { githubUrl } from "../pages/_app";
+
+export default function ExtensionButtons({
+  extensionName,
+}: {
+  extensionName: string;
+}) {
+  return (
+    <HStack spacing={8} className="not-prose">
+      <Button
+        size="lg"
+        color="white"
+        bgColor="#1967d2"
+        _hover={{ bgColor: "#165fc2" }}
+        _active={{ bgColor: "#1251a6" }}
+        as="a"
+        href={`https://console.firebase.google.com/project/_/extensions/install?ref=yaman/${extensionName}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Install in Console
+      </Button>
+
+      <Tooltip label="View Source Code on GitHub" openDelay={600}>
+        <Button
+          size="lg"
+          variant="link"
+          as="a"
+          href={`${githubUrl}/tree/main/packages/${extensionName}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </Button>
+      </Tooltip>
+    </HStack>
+  );
+}
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,11 +1,17 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import DocsLayout from "../components/docs-layout";
 import "../styles/globals.css";
 
+export const githubUrl =
+  "https://github.com/yamankatby/firestore-connect-document";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DocsLayout>
-      <Component {...pageProps} />
-    </DocsLayout>
+    <ChakraProvider>
+      <DocsLayout>
+        <Component {...pageProps} />
+      </DocsLayout>
+    </ChakraProvider>
   );
 }
