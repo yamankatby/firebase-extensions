e933251334b9924ab8290bf0997e0b28f50e3431
Yaman Katby <m.yaman.katby@gmail.com>
2022-11-24 15:52:57 +0300

    Revert "docs(*): setup mui and next font"
    This reverts commit dc4d6fe3fc4ca450ca59a385b9c7ae09cd92c49b.

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,16 +1,11 @@
-import { createTheme, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import DocsLayout from "../components/docs-layout";
 import "../styles/globals.css";
 
-const theme = createTheme();
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <DocsLayout>
-        <Component {...pageProps} />
-      </DocsLayout>
-    </ThemeProvider>
+    <DocsLayout>
+      <Component {...pageProps} />
+    </DocsLayout>
   );
 }
