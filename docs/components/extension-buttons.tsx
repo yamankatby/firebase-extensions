10bf9b8565dcb5905658e94dc79c44420795738f
Yaman Katby <m.yaman.katby@gmail.com>
2022-11-29 15:59:46 +0300

    docs: work on the buttons

--- docs/components/extension-buttons.tsx
+++ docs/components/extension-buttons.tsx
@@ -1,10 +1,13 @@
 import { Button, HStack, Tooltip } from "@chakra-ui/react";
+import Link from "next/link";
 import { githubUrl } from "../pages/_app";
 
 export default function ExtensionButtons({
   extensionName,
+  sourceCode = true,
 }: {
   extensionName: string;
+  sourceCode?: boolean;
 }) {
   return (
     <HStack spacing={8} className="not-prose">
@@ -19,19 +22,31 @@ export default function ExtensionButtons({
         Install in Console
       </Button>
 
-      <Tooltip label="View Source Code on GitHub" openDelay={600}>
+      {sourceCode ? (
+        <Tooltip label="View Source Code on GitHub" openDelay={600}>
+          <Button
+            size="lg"
+            variant="link"
+            colorScheme="brand"
+            as="a"
+            href={`${githubUrl}/tree/main/packages/${extensionName}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source Code
+          </Button>
+        </Tooltip>
+      ) : (
         <Button
           size="lg"
           variant="link"
-          as="a"
           colorScheme="brand"
-          href={`${githubUrl}/tree/main/packages/${extensionName}`}
-          target="_blank"
-          rel="noopener noreferrer"
+          as={Link}
+          href={extensionName}
         >
-          Source Code
+          Documentation
         </Button>
-      </Tooltip>
+      )}
     </HStack>
   );
 }
