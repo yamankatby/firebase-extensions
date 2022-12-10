3b4eb931c877d800617b93bed25080633cfb85cf
Yaman Katby <m.yaman.katby@gmail.com>
2022-12-10 18:16:27 +0300

    test(firestore-one-to-one): add env

--- packages/firestore-one-to-one/functions/__tests__/index.test.ts
+++ packages/firestore-one-to-one/functions/__tests__/index.test.ts
@@ -1,5 +1,11 @@
 import * as admin from "firebase-admin";
 
+process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+process.env.FIREBASE_FIRESTORE_EMULATOR_ADDRESS = "localhost:8080";
+process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099";
+process.env.PUBSUB_EMULATOR_HOST = "localhost:8085";
+process.env.GOOGLE_CLOUD_PROJECT = "demo-test";
+
 admin.initializeApp();
 
 const db = admin.firestore();
