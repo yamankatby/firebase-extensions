a5a878fc13bc63d7dae1b16cc43d64eabf65652c
Yaman Katby <m.yaman.katby@gmail.com>
2023-02-01 13:33:44 +0300

    docs: add og image

--- docs/app/head.tsx
+++ docs/app/head.tsx
@@ -1,13 +1,34 @@
+import React from "react";
+
 export default function Head() {
   return (
     <>
       <title>Yaman's Firebase Extensions</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="title" content="Yaman's Firebase Extensions" />
       <meta
         name="description"
         content="A set of Firebase extensions to help you build better apps."
       />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://extensions.work/" />
+      <meta property="og:title" content="Yaman's Firebase Extensions" />
+      <meta
+        property="og:description"
+        content="A set of Firebase extensions to help you build better apps."
+      />
+      <meta property="og:image" content="/og.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://extensions.work/" />
+      <meta property="twitter:title" content="Yaman's Firebase Extensions" />
+      <meta
+        property="twitter:description"
+        content="A set of Firebase extensions to help you build better apps."
+      />
+      <meta property="twitter:image" content="/og.png"></meta>
     </>
   );
 }
