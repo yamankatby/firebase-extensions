ee0cb8ee959f356b58ca6ee0e55118f3118ff9a1
Yaman Katby <m.yaman.katby@gmail.com>
2023-02-01 13:07:11 +0300

    docs: add gtag script

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -16,7 +17,20 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={`${color}`}>{children}</body>
+      <body className={`${color}`}>
+        {children}
+        <Script
+          strategy="beforeInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-D1CZXQ4CST"
+        />
+        <Script id="google-analytics" strategy="beforeInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D1CZXQ4CST');`}
+        </Script>
+      </body>
     </html>
   );
 }
