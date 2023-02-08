0d12b8c05af2ccbc73f32c67582e57f72a555366
Yaman Katby <m.yaman.katby@gmail.com>
2023-02-08 12:50:13 +0300

    feat(generate-og-image): remove static template param
    fixes: #6

--- generate-og-image/functions/src/utils.ts
+++ generate-og-image/functions/src/utils.ts
@@ -1,14 +1,4 @@
 import twemoji from "twemoji";
 
-export const isValidHttpUrl = (string: string) => {
-  let url;
-  try {
-    url = new URL(string);
-  } catch (_) {
-    return false;
-  }
-  return url.protocol === "http:" || url.protocol === "https:";
-};
-
 const twOptions = { folder: "svg", ext: ".svg" };
 export const emojify = (text: string) => twemoji.parse(text, twOptions);
