3d39fc00ff29f8628a2fdd17e351cf061e127ab4
Yaman Katby <m.yaman.katby@gmail.com>
2023-04-09 14:22:37 +0300

    feat(firestore-chatgpt): proof of concept

--- firestore-chatgpt/functions/src/index.ts
+++ firestore-chatgpt/functions/src/index.ts
@@ -9,18 +9,38 @@
  */
 
 import * as functions from "firebase-functions";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+});
+
+const openai = new OpenAIApi(configuration);
+
+interface Config {
+  apiKey: string;
+  collectionPath: string;
+  messagesFieldName: string;
+  systemMessage?: string;
+}
 
 exports.greetTheWorld = functions.https.onRequest(
-  (req: functions.Request, res: functions.Response) => {
-    // Here we reference a user-provided parameter
-    // (its value is provided by the user during installation)
-    const consumerProvidedGreeting = process.env.GREETING;
+  async (req: functions.Request, res: functions.Response) => {
+    const resp = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a JSON generator,You are a user name extractor, find the user name from the following text and response with a JSON object has only name key",
+        },
+        {
+          role: "user",
+          content:
+            "my name starts with Y and ends with n and in between is ama",
+        },
+      ],
+    });
 
-    // And here we reference an auto-populated parameter
-    // (its value is provided by Firebase after installation)
-    const instanceId = process.env.EXT_INSTANCE_ID;
-
-    const greeting = `${consumerProvidedGreeting} World from ${instanceId}`;
-
-    res.send(greeting);
-  });
+    res.json(resp.data);
+  }
+);
